e615977056a31f10ab13d8f83cb1c5389ce8a32c
German Nester <german_nester@mail.ru>
2025-01-05 16:46:23 -0600

    Initialize fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      noto: ["Noto Sans", "sans"],
+      lexend: ["Lexend Tera", "sans"],
+    },
   },
   plugins: [],
-}
-
+};
