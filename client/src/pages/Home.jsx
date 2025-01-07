91e5de278ffdcf682b80ac07d2962299ef01aad2
German Nester <german_nester@mail.ru>
2025-01-06 23:09:16 -0600

    Add baseline home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="bg-slate-50 min-h-screen flex flex-col">
+      <div className="bg-slate-50 h-16 drop-shadow-lg grid grid-cols-[1fr,_auto,_1fr]">
+        {/* Left bar corner */}
+        <div></div>
+        {/* Navigation buttons */}
+        <div className="flex justify-center items-center h-min-fit">
+          <div className="px-6">Home</div>
+          <div className="px-6">Decks</div>
+          <div className="px-6">About</div>
+        </div>
+        {/* Right bar corner */}
+        <div></div>
+      </div>
+    </div>
+  );
 }
