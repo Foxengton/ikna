eab437e3d6a130813ab47b0dd5b41f8f2b0cdd75
German Nester <german_nester@mail.ru>
2025-01-07 19:25:39 -0600

    Add page wrapper component

---
+++ client/src/features/PageWrapper.jsx
@@ -0,0 +1,11 @@
+import { Children } from "react";
+import NavigationBar from "./NavigationBar.jsx";
+
+export default function PageWrapper({ children }) {
+  return (
+    <div className="bg-slate-50 min-h-screen flex flex-col">
+      <NavigationBar />
+      {Children.toArray(children)}
+    </div>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
-import NavigationBar from "../features/NavigationBar.jsx";
+import PageWrapper from "../features/PageWrapper.jsx";
 
 export default function Home() {
   return (
-    <div className="bg-slate-50 min-h-screen flex flex-col">
-      <NavigationBar />
+    <PageWrapper>
       {/* Center logo */}
       <section className="flex flex-col justify-center items-center">
         <div className="text-8xl font-lexend mt-36 font-light">IKNA</div>
         <div className="text-2xl font-light">Learning is easy!</div>
       </section>
-    </div>
+    </PageWrapper>
   );
 }
