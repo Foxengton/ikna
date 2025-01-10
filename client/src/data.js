90551beac29f42168855350cb6d29d8e7293a5fe
German Nester <german_nester@mail.ru>
2025-01-09 19:49:47 -0600

    Add user test data

---
+++ client/src/data.js
@@ -0,0 +1,26 @@
+export default {
+  decks: [
+    {
+      title: "Nouns",
+      cards: [
+        { front: "table", back: "стол" },
+        { front: "char", back: "стул" },
+        { front: "pencil", back: "карандаш" },
+        { front: "cup", back: "чашка" },
+        { front: "room", back: "комната" },
+        { front: "door", back: "дверь" },
+        { front: "window", back: "окно" },
+      ],
+    },
+    {
+      title: "Verbs",
+      cards: [
+        { front: "make", back: "делать" },
+        { front: "work", back: "работать" },
+        { front: "eat", back: "есть" },
+        { front: "drink", back: "пить" },
+        { front: "write", back: "писать" },
+      ],
+    },
+  ],
+};
