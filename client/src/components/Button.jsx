c5231ae55911f1c7ff017d1c67d6952913b59631
German Nester <german_nester@mail.ru>
2025-01-11 15:51:21 -0600

    Add onClick to button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,12 @@
 import { Children } from "react";
 import { NavLink } from "react-router";
 
-export default function Button({ className, to = null, children }) {
+export default function Button({ className, to = null, onClick, children }) {
   return (
     <NavLink
       className={"whitespace-nowrap h-min px-4 py-2 rounded-lg " + className}
       to={to}
+      onClick={onClick}
     >
       {Children.toArray(children)}
     </NavLink>
