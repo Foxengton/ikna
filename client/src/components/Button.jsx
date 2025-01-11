b51b0a54dceae80d94daae8f0a3d404028465234
German Nester <german_nester@mail.ru>
2025-01-10 19:28:16 -0600

    Add navigation option to button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -5,6 +5,7 @@ export default function Button({ className, to = null, children }) {
   return (
     <NavLink
       className={"whitespace-nowrap h-min px-4 py-2 rounded-lg " + className}
+      to={to}
     >
       {Children.toArray(children)}
     </NavLink>
