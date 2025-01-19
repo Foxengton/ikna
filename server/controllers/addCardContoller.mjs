5add5d548d9dcbcf545d70ed9aadd21dd809767e
German Nester <german_nester@mail.ru>
2025-01-18 22:55:41 -0600

    Add card creation controller

---
+++ server/controllers/addCardContoller.mjs
@@ -0,0 +1,59 @@
+import { pool } from "../app.mjs";
+import jwtVerify from "../services/jwtVerify.mjs";
+
+export default async function addCardContoller(req, res) {
+  /*
+    ======= Add card =======
+    Expected object: {
+        token: token,
+        data: {
+          deckName: deckName
+          cardFront: front
+          cardBack: back
+        }
+    }
+  */
+  req = req?.body;
+  const tokenUsername = jwtVerify(req?.token)?.username;
+  const deckName = req?.data?.deckName;
+  const cardFront = req?.data?.cardFront;
+  const cardBack = req?.data?.cardBack;
+  // Checking token
+  if (!tokenUsername) {
+    res.status(401).send("Access unauthorized");
+    return;
+  }
+  // Checking deck name
+  if (!deckName) {
+    res.status(404).send("Deck name missing");
+    return;
+  }
+  let query = "SELECT id FROM users WHERE username = ?";
+  let [result] = await pool.query(query, [tokenUsername]);
+  // User not found
+  if (result.length === 0) {
+    res.status(400).send(`User ${tokenUsername} doesn't exist`);
+    return;
+  }
+  const userId = result[0].id;
+  query = "SELECT * FROM decks WHERE userId = ? AND deckName = ?";
+  [result] = await pool.query(query, [userId, deckName]);
+  // Checking decks with the same name
+  if (result.length === 0) {
+    res.status(400).send(`Deck named ${deckName} doesn't exist`);
+    return;
+  }
+  const deckId = result[0].id;
+  // Adding card
+  query =
+    "INSERT INTO cards (userId, deckId, cardFront, cardBack, lastReview, nextInterval) VALUES (?, ?, ?, ?, ?, ?)";
+  await pool.query(query, [
+    userId,
+    deckId,
+    cardFront,
+    cardBack,
+    "1970-01-01 00:00:01",
+    24 * 60 * 60 * 1000, // Initial interval = one day
+  ]);
+  res.status(200).send("Card created");
+}
--- server/services/initControllers.mjs
+++ server/services/initControllers.mjs
@@ -4,11 +4,13 @@ import registrationController from "../controllers/registrationContoller.mjs";
 import loginController from "../controllers/loginContoller.mjs";
 import addDeckContoller from "../controllers/addDeckContoller.mjs";
 import deleteDeckContoller from "../controllers/deleteDeckContoller.mjs";
+import addCardContoller from "../controllers/addCardContoller.mjs";
 
 export default async function initControllers() {
   app.use(express.json());
   app.get("/api/register", (req, res) => registrationController(req, res));
   app.get("/api/login", (req, res) => loginController(req, res));
-  app.get("/api/decks/add", (req, res) => addDeckContoller(req, res));
-  app.get("/api/decks/delete", (req, res) => deleteDeckContoller(req, res));
+  app.get("/api/deck/add", (req, res) => addDeckContoller(req, res));
+  app.get("/api/deck/delete", (req, res) => deleteDeckContoller(req, res));
+  app.get("/api/card/add", (req, res) => addCardContoller(req, res));
 }
