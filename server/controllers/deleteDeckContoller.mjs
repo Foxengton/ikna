c35dad95e76dd02df046c9d91ce14f917c2c0844
German Nester <german_nester@mail.ru>
2025-01-18 22:03:16 -0600

    Add deck deletion controller

---
+++ server/controllers/deleteDeckContoller.mjs
@@ -0,0 +1,46 @@
+import { pool } from "../app.mjs";
+import jwtVerify from "../services/jwtVerify.mjs";
+
+export default async function deleteDeckContoller(req, res) {
+  /*
+    ======= Add card =======
+    Expected object: {
+        token: token,
+        data: {
+          deckName: deckName
+        }
+    }
+  */
+  req = req?.body;
+  const tokenUsername = jwtVerify(req?.token)?.username;
+  const deckName = req?.data?.deckName;
+  // Checking token
+  if (!tokenUsername) {
+    res.status(401).send("Access unauthorized");
+    return;
+  }
+  // Checking deck name
+  if (!deckName) {
+    res.status(404).send("Deck name missing");
+    return;
+  }
+  let query = "SELECT id FROM users WHERE username = ?";
+  let [result] = await pool.query(query, [tokenUsername]);
+  // User not found
+  if (result.length === 0) {
+    res.status(400).send("Username doesn't exist");
+    return;
+  }
+  const userId = result[0].id;
+  query = "SELECT * FROM decks WHERE userId = ? AND deckName = ?";
+  [result] = await pool.query(query, [userId, deckName]);
+  // Checking if deck was deleted
+  if (result.length === 0) {
+    res.status(400).send(`Deck named ${deckName} doesn't exist`);
+    return;
+  }
+  // Deck delection
+  query = "DELETE FROM decks WHERE userId = ? AND deckName = ?";
+  await pool.query(query, [userId, deckName]);
+  res.status(200).send("Deck deleted");
+}
--- server/services/initControllers.mjs
+++ server/services/initControllers.mjs
@@ -3,10 +3,12 @@ import express from "express";
 import registrationController from "../controllers/registrationContoller.mjs";
 import loginController from "../controllers/loginContoller.mjs";
 import addDeckContoller from "../controllers/addDeckContoller.mjs";
+import deleteDeckContoller from "../controllers/deleteDeckContoller.mjs";
 
 export default async function initControllers() {
   app.use(express.json());
   app.get("/api/register", (req, res) => registrationController(req, res));
   app.get("/api/login", (req, res) => loginController(req, res));
   app.get("/api/decks/add", (req, res) => addDeckContoller(req, res));
+  app.get("/api/decks/delete", (req, res) => deleteDeckContoller(req, res));
 }
