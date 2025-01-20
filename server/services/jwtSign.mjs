65495dcc664299d763ca248c5aabc73d15594405
German Nester <german_nester@mail.ru>
2025-01-19 19:30:58 -0600

    Fix secret key being passed as utf-8 instead of base64

--- server/services/jwtSign.mjs
+++ server/services/jwtSign.mjs
@@ -2,7 +2,7 @@ import { config, secretKey } from "../app.mjs";
 import jwt from "jsonwebtoken";
 
 export default function jwtSign(data) {
-  return jwt.sign(data, secretKey, {
+  return jwt.sign(data, Buffer.from(secretKey, "base64"), {
     algorithm: "HS256",
     expiresIn: config.jwtExpirationTime,
   });
