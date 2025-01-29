7ce68700e5455082c84bbc365a7077bdfcee14e4
German Nester <german_nester@mail.ru>
2025-01-29 11:11:04 -0600

    Configure jest from modules

---
+++ server/tests/controllers.test.js
@@ -0,0 +1,15 @@
+import registrationController from "../controllers/registrationContoller.mjs";
+import loginController from "../controllers/loginContoller.mjs";
+import addDeckContoller from "../controllers/addDeckContoller.mjs";
+import deleteDeckContoller from "../controllers/deleteDeckContoller.mjs";
+import addCardContoller from "../controllers/addCardContoller.mjs";
+import deleteCardContoller from "../controllers/deleteCardContoller.mjs";
+import listDecksContoller from "../controllers/listDecksContoller.mjs";
+import listCardsContoller from "../controllers/listCardsContoller.mjs";
+import updateCardContoller from "../controllers/updateCardContoller.mjs";
+import updateDeckContoller from "../controllers/updateDeckContoller.mjs";
+import reviewCardContoller from "../controllers/reviewCardContoller.mjs";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(1 + 2).toBe(3);
+});
