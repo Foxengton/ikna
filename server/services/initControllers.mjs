f85508d9afb326e3af497c6b2d0453d19a778d39
German Nester <german_nester@mail.ru>
2025-02-02 22:54:56 -0600

    Change controller methods according to REST methodology

--- server/services/initControllers.mjs
+++ server/services/initControllers.mjs
@@ -16,13 +16,13 @@ export default async function initControllers() {
   app.use(express.json());
   app.post("/api/register", (req, res) => registrationController(req, res));
   app.post("/api/login", (req, res) => loginController(req, res));
-  app.post("/api/deck/list", (req, res) => listDecksContoller(req, res));
+  app.get("/api/deck/list", (req, res) => listDecksContoller(req, res));
   app.post("/api/deck/add", (req, res) => addDeckContoller(req, res));
-  app.post("/api/deck/update", (req, res) => updateDeckContoller(req, res));
-  app.post("/api/deck/delete", (req, res) => deleteDeckContoller(req, res));
+  app.patch("/api/deck/update", (req, res) => updateDeckContoller(req, res));
+  app.delete("/api/deck/delete", (req, res) => deleteDeckContoller(req, res));
   app.post("/api/card/add", (req, res) => addCardContoller(req, res));
-  app.post("/api/card/update", (req, res) => updateCardContoller(req, res));
-  app.post("/api/card/delete", (req, res) => deleteCardContoller(req, res));
-  app.post("/api/card/list", (req, res) => listCardsContoller(req, res));
-  app.post("/api/card/review", (req, res) => reviewCardContoller(req, res));
+  app.patch("/api/card/update", (req, res) => updateCardContoller(req, res));
+  app.delete("/api/card/delete", (req, res) => deleteCardContoller(req, res));
+  app.get("/api/card/list", (req, res) => listCardsContoller(req, res));
+  app.patch("/api/card/review", (req, res) => reviewCardContoller(req, res));
 }
