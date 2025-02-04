02a2d989f6969c3f9748528e389cc3e51bb075aa
German Nester <german_nester@mail.ru>
2025-02-03 19:25:17 -0600

    Add jwt passing to authorization header

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
 import config from "../config.json";
+import Cookies from "js-cookie";
+import { useContext } from "react";
 
-export default async function api(method, url, data = {}) {
+const token = JSON.parse(Cookies.get("user-data") ?? "null")?.token;
+axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+axios.defaults.baseURL = config.serverBaseUrl;
+
+export default async function api(method, url, data = null) {
   const result = await axios({
     method: method,
-    baseURL: config.serverBaseUrl,
     url: url,
     data: data,
   }).catch((error) => {
