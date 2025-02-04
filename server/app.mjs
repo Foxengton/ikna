0f72b35e7547e55807fa312e7c098cd1b8fff970
German Nester <german_nester@mail.ru>
2025-02-03 19:40:10 -0600

    Add database name to pool initialization

--- server/app.mjs
+++ server/app.mjs
@@ -17,6 +17,7 @@ export const config = await JSON.parse(
 export const pool = mysql.createPool({
   host: config.db.host,
   user: config.db.user,
+  database: config.db.name,
   password: config.db.password,
   multipleStatements: true,
 });
