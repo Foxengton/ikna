877c2f9b42885539018d980e1c379c5768b03fb3
German Nester <german_nester@mail.ru>
2025-02-04 19:54:17 -0600

    Update tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      noto: ["Noto Sans", "sans"],
-      lexend: ["Lexend Tera", "sans"],
-    },
-  },
   plugins: [],
 };
