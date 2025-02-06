b260b936656d3f5d5ee2fde39116c9b274da61a5
German Nester <german_nester@mail.ru>
2025-02-05 18:52:38 -0600

    Switch id with guid in card list controller

--- server/controllers/listCardsContoller.mjs
+++ server/controllers/listCardsContoller.mjs
@@ -49,7 +49,7 @@ export default async function listCardsContoller(req, res) {
     query = `
       SELECT JSON_ARRAYAGG(
         JSON_OBJECT(
-          'id', id, 'cardFront', card_front, 'cardBack', card_back
+          'guid', guid, 'cardFront', card_front, 'cardBack', card_back
         )
       ) AS data
       FROM cards
