989ca5ae21c95cfaef5e5929ca769f9986becc6b
German Nester <german_nester@mail.ru>
2025-02-10 01:07:54 -0600

    Display next estimated interval for each verdict when studying cards

--- client/src/pages/DeckStudy.jsx
+++ client/src/pages/DeckStudy.jsx
@@ -5,29 +5,40 @@ import Button from "../components/Button.jsx";
 import api from "../services/api.jsx";
 import DeckInfoHeader from "../components/DeckInfoHeader.jsx";
 import Card from "../components/Card.jsx";
+import moment from "moment";
 
-const controlButtons = [
+const CONTROL_BUTTONS = [
   {
     text: "Again",
     verdict: "Again",
+    multiplier: 0,
     bg: "bg-red-300",
+    learningDisplay: true,
   },
   {
     text: "Hard",
     verdict: "Hard",
+    multiplier: 1.25,
     bg: "bg-orange-300",
+    learningDisplay: false,
   },
   {
     text: "Good",
     verdict: "Good",
+    multiplier: 1.75,
     bg: "bg-green-300",
+    learningDisplay: true,
   },
   {
     text: "Easy",
     verdict: "Easy",
+    multiplier: 2.25,
     bg: "bg-teal-300",
+    learningDisplay: false,
   },
 ];
+
+const LEARNING_STEPS = [{ m: 1 }, { m: 5 }, { d: 1 }];
 
 export default function DeckStudy() {
   let navigate = useNavigate();
@@ -61,6 +72,13 @@ export default function DeckStudy() {
     fetchDeckData();
   }
 
+  // Return the next estimated interval depedning on verdict and card status
+  function verdictTimeEstimation(interval, multiplier, status, step) {
+    if (multiplier == 0) return 0;
+    if (status === "LEARNING") return LEARNING_STEPS[step];
+    if (status === "MEMORIZING") return interval * multiplier;
+  }
+
   if (!deckData) return <PageWrapper />;
   return (
     <PageWrapper>
@@ -72,9 +90,24 @@ export default function DeckStudy() {
           </section>
           {/* Card control buttons */}
           <section className="flex gap-3 justify-center">
-            {controlButtons.map((btn) => (
+            {CONTROL_BUTTONS.filter(
+              (btn) =>
+                cardList[0].status === "MEMORIZING" || btn.learningDisplay
+            ).map((btn) => (
               <div className="flex flex-col items-center" key={btn.text}>
-                <span className="text-sm">1 days</span>
+                <span className="text-sm">
+                  {moment
+                    .duration(
+                      verdictTimeEstimation(
+                        cardList[0].currentInterval,
+                        btn.multiplier,
+                        cardList[0].status,
+                        cardList[0].learningStep
+                      ),
+                      "seconds"
+                    )
+                    .humanize()}
+                </span>
                 <Button
                   className={"mt-1 min-w-20 flex justify-center " + btn.bg}
                   onClick={() => handleVerdict(btn.verdict)}
