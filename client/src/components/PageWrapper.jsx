94558021bfb14ce780aeec7be42d8b5dfeaa984b
German Nester <german_nester@mail.ru>
2025-02-24 21:53:13 -0600

    Add redirect option for unauthorized users to page wrapper

--- client/src/components/PageWrapper.jsx
+++ client/src/components/PageWrapper.jsx
@@ -1,10 +1,27 @@
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router";
 import NavigationBar from "../components/NavigationBar.jsx";
+import { AuthContext } from "../contexts/AuthProvider.jsx";
 
-export default function PageWrapper({ children }) {
+export default function PageWrapper({ children, authRedirect = "" }) {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useContext(AuthContext);
+  const username = userData?.username;
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    if (authRedirect && !username) {
+      navigate(authRedirect);
+    } else setShowContent(true);
+  }, []);
+
   return (
     <div className="relative bg-slate-50 min-h-screen flex flex-col font-noto">
       <NavigationBar />
-      {children}
+      {showContent ? children : null}
     </div>
   );
 }
