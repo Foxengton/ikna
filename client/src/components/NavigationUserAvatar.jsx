f7c175e8b9dfddbfed0c8662891fe91d9afaa08a
German Nester <german_nester@mail.ru>
2025-02-28 23:49:01 -0600

    Refactor user avatar to use radix

--- client/src/components/NavigationUserAvatar.jsx
+++ client/src/components/NavigationUserAvatar.jsx
@@ -1,8 +1,12 @@
+import { Avatar } from "radix-ui";
+
 export default function NavigationUserAvatar({ username, className }) {
   return (
-    <img
-      className={`rounded-full ${className}`}
-      src={`https://ui-avatars.com/api/?background=ffdf20&length=1&bold=true&name=${username}`}
-    />
+    <Avatar.Root>
+      <Avatar.Image
+        className={`rounded-full ${className}`}
+        src={`https://ui-avatars.com/api/?background=ffdf20&length=1&bold=true&name=${username}`}
+      />
+    </Avatar.Root>
   );
 }
