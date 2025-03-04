e5d64f6cfddc4548094c3824f1ee55ae0db76b6b
German Nester <german_nester@mail.ru>
2025-03-03 21:20:08 -0600

    Add auth check on the first site load

--- client/src/contexts/AuthProvider.jsx
+++ client/src/contexts/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import Cookies from "js-cookie";
 import { useEffect } from "react";
+import api from "../services/api.jsx";
 
 export const AuthContext = createContext();
 
@@ -13,6 +14,16 @@ export default function AuthProvider({ children }) {
   useEffect(() => {
     Cookies.set("user-data", JSON.stringify(userData), { expires: 365 });
   }, [userData]);
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
+  // Login attempt on the first load
+  async function fetchToken() {
+    const result = await api("post", "/login", userData.token);
+    setUserData(result?.data ?? null);
+  }
 
   return (
     <AuthContext.Provider value={[userData, setUserData]}>
